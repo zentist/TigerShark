70f0d5de936aca58bd7dc2fb05a20e2c3eeab2fe
Dave Peticolas <dpeticolas@counsyl.com>
2014-10-01 12:06:14 -0700

    Refactor test file defs.

--- tests/test_control.py
+++ tests/test_control.py
@@ -2,37 +2,12 @@ import datetime
 import os
 import unittest
 
-from tigershark import X12_4010_X059
-from tigershark import X12_4010_X091A1
 from tigershark import X12_4010_X092A1
-from tigershark import X12_4010_X093A1
-from tigershark import X12_4010_X096A1
 from tigershark import X12_5010_X221A1
 from tigershark.facade.control import ControlHeaders
 from tigershark.parsers import parse_control_headers
 
-
-# Map (X12VersionTuple, Transaction Set Identifier) to test file names.
-TEST_FILE_MAP = {
-    (X12_4010_X092A1, '271'): '271-dependent-benefits.txt',
-    (X12_4010_X092A1, '271'): '271-example-2.txt',
-    (X12_4010_X092A1, '271'): '271-example-dependent-rejection.txt',
-    (X12_4010_X092A1, '271'): '271-example.txt',
-    (X12_4010_X092A1, '271'): '271-related-entity.txt',
-    (X12_4010_X093A1, '276'): 'TEST 276 TXNs.txt',
-    (X12_4010_X059, '278'): 'TEST 278_13 TXNS.txt',
-    (X12_4010_X059, '278'): 'TEST 278_28 TXNS_SOA.txt',
-    (X12_4010_X091A1, '835'): '835-example-2.txt',
-    (X12_4010_X091A1, '835'): '835-example.txt',
-    (X12_4010_X096A1, '837'): '837-example.txt',
-    (X12_4010_X096A1, '837'): '837I-Examples.txt',
-    (X12_4010_X096A1, '837'): '837I-Patient-NotSubscriber.txt',
-    (X12_4010_X096A1, '837'): '837I-Patient-NotSubscriber2.txt',
-    (X12_4010_X096A1, '837'): '837I-Patient-Subscriber.txt',
-    (X12_5010_X221A1, '835'): '5010-835-example-1.txt',
-    (X12_5010_X221A1, '835'): '5010-835-example-2.txt',
-    (X12_5010_X221A1, '835'): '5010-835-example-3.txt',
-}
+from tests import TEST_FILE_MAP
 
 
 class TestControlHeaders(unittest.TestCase):
